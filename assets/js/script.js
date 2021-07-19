b6292ba0df0d1e1e7cd6cc7df021461252f8353e
Gui Barreto <barretogui90@gmail.com>
2021-07-18 21:22:12 -0300

    adicionando localStorage

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+const form = document.forms.cadastro;
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const {firstName, lastName, email} = form; 
+    console.log(firstName.value, lastName.value, email.value)
+    localStorage.setItem('cadastro', JSON.stringify({firstName:firstName.value, lastName:lastName.value, email:email.value})) 
+
+    form.reset()
+})
+
+
+
+
